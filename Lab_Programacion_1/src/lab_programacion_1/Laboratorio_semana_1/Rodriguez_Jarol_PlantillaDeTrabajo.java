23151a9a9961015087b8d03ac9f8f0afbd9d07d9
Jarol Rodriguez <102051629+bookerdew1@users.noreply.github.com>
2025-04-25 09:29:25 -0600

    Update Rodriguez_Jarol_PlantillaDeTrabajo.java

--- Lab_Programacion_1/src/lab_programacion_1/Laboratorio_semana_1/Rodriguez_Jarol_PlantillaDeTrabajo.java
+++ Lab_Programacion_1/src/lab_programacion_1/Laboratorio_semana_1/Rodriguez_Jarol_PlantillaDeTrabajo.java
@@ -5,10 +5,33 @@
  */
 package lab_programacion_1.Laboratorio_semana_1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Booker
  */
 public class Rodriguez_Jarol_PlantillaDeTrabajo {
+    
+    public static void main(String[] args) {
+        Scanner lea = new Scanner(System.in);
+        
+        System.out.println("Ingrese el nombre del empleado: ");
+        String nombre = lea.nextLine();
+        
+        System.out.println("Ingrese las hora trabajdas mensualmente: ");
+        int Htrabajadas = lea.nextInt();
+        
+        System.out.println("Ingrese la tarifa por hora de trabajo: ");
+        double tarifa = lea.nextDouble();
+        
+        double salario = (Htrabajadas * tarifa)/4;
+        
+        System.out.println("**** Boleta De Trabajo **** "
+                + "\nNombre del Empleado: "+ nombre
+                + "\nHoras Trabajas mensualmente: " + Htrabajadas
+                + "\nTarifa por hora de trabajo: Lps"+ tarifa 
+                + " \nSalario del empleado semanalmente: Lps" + salario);
+    }
     
 }
